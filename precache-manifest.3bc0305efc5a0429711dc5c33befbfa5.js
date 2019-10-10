d21e7ef0572fb55d6d862cdf327a120a89d7e588
alliejant <alliejant@gmail.com>
2019-10-10 11:51:44 -0700

    Updates

---
+++ precache-manifest.3bc0305efc5a0429711dc5c33befbfa5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5263e8abefbce7acc9eb3c44c2a72ad4",
+    "url": "/static/media/facepalm.5263e8ab.png"
+  },
+  {
+    "revision": "3447b84d873af5faee7c",
+    "url": "/static/css/main.79f8a7b6.chunk.css"
+  },
+  {
+    "revision": "93a4fc2ef71feed95e75",
+    "url": "/static/js/1.93a4fc2e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "201b0968097ebc36a5c035c8871da6cd",
+    "url": "/static/media/javascript-joke.201b0968.jpg"
+  },
+  {
+    "revision": "3447b84d873af5faee7c",
+    "url": "/static/js/main.3447b84d.chunk.js"
+  },
+  {
+    "revision": "7ce0a8b4bc0cd09a9a1486581d99fdfd",
+    "url": "/static/media/Alexandra-Garcia-Resume.7ce0a8b4.pdf"
+  },
+  {
+    "revision": "9aa33b993de40b1b08ccb8286a3e67e8",
+    "url": "/static/media/shrug.9aa33b99.png"
+  },
+  {
+    "revision": "bac9b1729c3bbeeaacd7a1aee05113ba",
+    "url": "/static/media/milky-way.bac9b172.jpg"
+  },
+  {
+    "revision": "d8c588c9a03f9654675fd0de990b094d",
+    "url": "/static/media/headshot.d8c588c9.jpg"
+  },
+  {
+    "revision": "06e0767aef072db71ccbfa0e2245aff7",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.606a047c4ef880c98dfe0a2ae0423530.js"
+  "/precache-manifest.3bc0305efc5a0429711dc5c33befbfa5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
