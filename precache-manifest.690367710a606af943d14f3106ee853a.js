a704599c5e0fd11656158ab7309ee1d131c87ce7
alliejant <alliejant@gmail.com>
2019-10-10 11:25:50 -0700

    Updates

---
+++ precache-manifest.690367710a606af943d14f3106ee853a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5263e8abefbce7acc9eb3c44c2a72ad4",
+    "url": "/static/media/facepalm.5263e8ab.png"
+  },
+  {
+    "revision": "f59c1d328ad4e103d2a0",
+    "url": "/static/css/main.84294d5d.chunk.css"
+  },
+  {
+    "revision": "93a4fc2ef71feed95e75",
+    "url": "/static/js/1.93a4fc2e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "201b0968097ebc36a5c035c8871da6cd",
+    "url": "/static/media/javascript-joke.201b0968.jpg"
+  },
+  {
+    "revision": "f59c1d328ad4e103d2a0",
+    "url": "/static/js/main.f59c1d32.chunk.js"
+  },
+  {
+    "revision": "7ce0a8b4bc0cd09a9a1486581d99fdfd",
+    "url": "/static/media/Alexandra-Garcia-Resume.7ce0a8b4.pdf"
+  },
+  {
+    "revision": "9aa33b993de40b1b08ccb8286a3e67e8",
+    "url": "/static/media/shrug.9aa33b99.png"
+  },
+  {
+    "revision": "bac9b1729c3bbeeaacd7a1aee05113ba",
+    "url": "/static/media/milky-way.bac9b172.jpg"
+  },
+  {
+    "revision": "d8c588c9a03f9654675fd0de990b094d",
+    "url": "/static/media/headshot.d8c588c9.jpg"
+  },
+  {
+    "revision": "013612f9e1964de609538fb0caeb18a9",
+    "url": "/index.html"
+  }
+];
